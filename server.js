44aabb343b24f297bc4aaed9fc9068c26983aeba
Helder <heldrida@gmail.com>
2016-09-18 02:35:55 +0100

    a few amends; heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT ? process.env.PORT : 8080;
+const dist = path.join(__dirname, 'build');
+
+app.use(express.static(dist));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(dist, 'index.html'));
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error); // eslint-disable-line no-console
+  }
+  console.info('Express is listening on port %s.', port); // eslint-disable-line no-console
+});
